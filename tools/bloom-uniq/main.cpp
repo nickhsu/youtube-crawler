007eec438aff7af55bd049a7b8e46e637e930e97
Nick Hsu <nickhsu.tw@gmail.com>
2011-03-10 14:13:57 +0800

    add bloom-uniq command args

--- tools/bloom-uniq/main.cpp
+++ tools/bloom-uniq/main.cpp
@@ -1,42 +1,47 @@
-#include <iostream>
-#include <string>
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include "bloom_filter.hpp"
 
-
-using namespace std;
-
-const int FILTER_SIZE = 100000;
-const float ERROR_RATE = 0.01;
 const int BUF_SIZ = 4096;
 
-int main(int argc, const char *argv[]) {
-	srand (time(NULL));
-	bloom_filter filter(FILTER_SIZE, ERROR_RATE, rand());
+void usage() {
+		printf("usage: bloom-uniq -e error_rate -n num_item < file_name\n");
+}
 
+int main(int argc, char *argv[]) {
+	int num_item, ch;
+	float error_rate;
+
+	while ((ch = getopt(argc, argv, "e:n:")) != -1) {
+		switch (ch) {
+		case 'e':
+			error_rate = atof(optarg);
+			break;
+		case 'n':
+			num_item = atoi(optarg);
+			break;
+		case '?':
+		default:
+			usage();
+			exit(1);
+			break;
+		}
+	}
+
+	srand (time(NULL));
+	bloom_filter filter(num_item, error_rate, rand());
 
 	char buffer[BUF_SIZ];
 	while (fgets(buffer, BUF_SIZ, stdin)) {
 		int len = strlen(buffer);
 		if (filter.contains(buffer, len) == false) {
-			cout << buffer << endl;
+			puts(buffer);
 			filter.insert(buffer, len);
 		}
 	}
-
-	/*
-	string buffer;
-	while (getline(cin, buffer)) {
-		if (filter.contains(buffer) == false) {
-			cout << buffer << endl;
-			filter.insert(buffer);
-		}
-	}
-	*/
-
 
 	return 0;
 }
